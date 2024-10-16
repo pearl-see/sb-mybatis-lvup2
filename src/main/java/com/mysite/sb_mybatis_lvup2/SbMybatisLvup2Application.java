aea2cb0aa4d14413cabf0013879ecd9f57753fa0
pearl-see <kjj0837@naver.com>
2024-10-17 00:22:01 +0900

    2프로젝트 설정

---
+++ src/main/java/com/mysite/sb_mybatis_lvup2/SbMybatisLvup2Application.java
@@ -0,0 +1,13 @@
+package com.mysite.sb_mybatis_lvup2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMybatisLvup2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMybatisLvup2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mysite/sb_mybatis_lvup2/SbMybatisLvup2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mysite.sb_mybatis_lvup2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbMybatisLvup2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
